be1df6605ac365395b0bd680795220756044a924
李仲澄 <zhgchgli@gmail.com>
2018-06-18 20:02:37 +0800

    Initial Commit

---
+++ ZWeatherAppTests/ZWeatherAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  ZWeatherAppTests.swift
+//  ZWeatherAppTests
+//
+//  Created by 李仲澄 on 2018/6/18.
+//  Copyright © 2018年 李仲澄. All rights reserved.
+//
+
+import XCTest
+@testable import ZWeatherApp
+
+class ZWeatherAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
